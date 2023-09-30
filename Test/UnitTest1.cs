dab7ba36f44b61e77a096bf0cf3419de97497c2f
Andrea Baravelli <ANDREA.BARAVELLI@local.ittcesena.it>
2023-09-30 09:34:15 +0200

    added database standard file, modified Test[StartUp]

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -5,8 +5,9 @@ namespace Test
     public class Tests
     {
         BusinessLayer bl;
+
         string dbCampione = @"..\..\SchoolGrades\bin\Debug\net6.0-windows\Data\SchoolGrades_StandardDb.sqlite";
-        string dbTest = @"..\..\SchoolGrades\bin\Debug\net6.0-windows\Data\SchoolGrades_TestDb.sqlite";
+        string dbTest = @"..\..\..\SchoolGrades_TestDb.sqlite";
 
         [SetUp]
         public void Setup()
